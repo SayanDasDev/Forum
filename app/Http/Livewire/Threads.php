7e48e95d04c64a2f42c0eea1ccec366d20a775eb
SayanDasDev <mesayan19@gmail.com>
2023-06-10 17:26:13 +0530

    store done

--- app/Http/Livewire/Threads.php
+++ app/Http/Livewire/Threads.php
@@ -4,21 +4,43 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Thread;
+
 use Illuminate\Support\Facades\Auth;
 
 class Threads extends Component
 {
-    public $th;
+    public $th, $thread_title, $thread_subtitle, $thread_content;
     public $threadShowing=true;
     public function render()
     {
         $userId = Auth::id();
-        $this->th = Thread::where('user_id', $userId)->get();
+        $this->th = Thread::where('user_id', $userId)
+            ->orderBy('id', 'desc')
+            ->get();
         return view('livewire.threads');
     }
 
     public function enterNewThreadMode()
     {
         $this->threadShowing=false;
+    }
+
+    public function store()
+    {
+        $userId = Auth::id();
+        $this->validate([
+            'thread_title' => 'required|max:255',
+            'thread_subtitle' => 'nullable|max:255',
+            'thread_content' => 'required',
+        ]);
+
+        Thread::create([
+            'thread_title' => $this->thread_title,
+            'thread_subtitle' => $this->thread_subtitle,
+            'thread_content' => $this->thread_content,
+            'user_id' => $userId,
+        ]);
+
+        return redirect('/threads');
     }
 }
--- resources/views/livewire/threadNew.blade.php
+++ resources/views/livewire/threadNew.blade.php
@@ -14,7 +14,7 @@
                             <label class="block font-medium text-gray-700 dark:text-gray-300 text-xl px-2" for="thread_title">
                                 Heading
                             </label>
-                            <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block mx-2 w-full" id="thread_title" name="thread_title" type="text">
+                            <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block mx-2 w-full" id="thread_title" name="thread_title" wire:model="thread_title" type="text">
                             {{-- <x-input-error :messages="$errors->addThreadTitle->get('ThreadTitle')" class="mt-2" /> --}}
                         </div>
 
@@ -22,20 +22,20 @@
                             <label class="block font-medium text-sm text-gray-700 dark:text-gray-300 px-2" for="thread_subtitle">
                                 Sub Heading <span class="text-gray-600">(Optional)</span>
                             </label>
-                            <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block mx-2 w-full" id="thread_subtitle" name="thread_subtitle" type="text">
+                            <input class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block mx-2 w-full" id="thread_subtitle" name="thread_subtitle" wire:model="thread_subtitle" type="text">
                             {{-- <x-input-error :messages="$errors->addThreadSubTitle->get('ThreadSubTitle')" class="mt-2" /> --}}
                         </div>
 
                         <div>
                             
                             <label for="thread_content" class="block font-medium text-sm text-gray-700 dark:text-gray-300 px-2">Content</label>
-                            <textarea id="thread_content" rows="8" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block mx-2 w-full" placeholder="Your Content Here..."></textarea>
+                            <textarea id="thread_content" rows="8" class="border-gray-300 dark:border-gray-700 dark:bg-gray-900 dark:text-gray-300 focus:border-indigo-500 dark:focus:border-indigo-600 focus:ring-indigo-500 dark:focus:ring-indigo-600 rounded-md shadow-sm mt-1 block mx-2 w-full" placeholder="Your Content Here..." wire:model="thread_content"></textarea>
                             {{-- <x-input-error :messages="$errors->addThreadContent->get('ThreadContent')" class="mt-2" /> --}}
 
                         </div>
 
                         <div class="flex justify-end pr-6 gap-4">
-                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150">
+                            <button type="submit" class="inline-flex items-center px-4 py-2 bg-gray-800 dark:bg-gray-200 border border-transparent rounded-md font-semibold text-xs text-white dark:text-gray-800 uppercase tracking-widest hover:bg-gray-700 dark:hover:bg-white focus:bg-gray-700 dark:focus:bg-white active:bg-gray-900 dark:active:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 dark:focus:ring-offset-gray-800 transition ease-in-out duration-150" wire:click.prevent="store()">
                                 Post Thread
                             </button>
                         </div>
