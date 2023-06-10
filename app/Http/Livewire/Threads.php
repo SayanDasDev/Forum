0a0077e5a719e63d7df7b50d698cf5f01ae6d214
SayanDasDev <mesayan19@gmail.com>
2023-06-10 11:31:18 +0530

    now it only shows the threads of currently logged in user.

--- app/Http/Livewire/Threads.php
+++ app/Http/Livewire/Threads.php
@@ -4,13 +4,15 @@ namespace App\Http\Livewire;
 
 use Livewire\Component;
 use App\Models\Thread;
+use Illuminate\Support\Facades\Auth;
 
 class Threads extends Component
 {
     public $th;
     public function render()
     {
-        $this->th = Thread::all();
+        $userId = Auth::id();
+        $this->th = Thread::where('user_id', $userId)->get();
         return view('livewire.threads');
     }
 }
