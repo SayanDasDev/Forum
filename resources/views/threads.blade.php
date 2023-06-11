6e5c266c86a5da1c801c7e2166e0449f5b5ae875
SayanDasDev <mesayan19@gmail.com>
2023-06-12 00:50:13 +0530

    ...

--- resources/views/threads.blade.php
+++ resources/views/threads.blade.php
@@ -4,14 +4,6 @@
             <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">
                 {{ __('My Threads') }}
             </h2>
-            {{-- @if($threadShowing)
-                <div class="ml-auto">
-                    <button wire:click="enterNewThreadMode" class="inline-flex items-center h-10 px-5 text-indigo-100 transition-colors duration-150 bg-red-500 rounded-lg focus:shadow-outline hover:bg-red-700">
-                    <svg class="w-4 h-4 fill-current" viewBox="0 0 20 20"><path d="M10 3a1 1 0 011 1v5h5a1 1 0 110 2h-5v5a1 1 0 11-2 0v-5H4a1 1 0 110-2h5V4a1 1 0 011-1z" clip-rule="evenodd" fill-rule="evenodd"></path></svg>
-                    <span class="ml-2">New Thread</span>
-                    </button>
-                </div>
-            @endif --}}
         </div>
     </x-slot>
 
