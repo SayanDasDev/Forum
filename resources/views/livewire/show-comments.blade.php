d6f808daef45d1eb364794a844f5db909f6f15e7
SayanDasDev <mesayan19@gmail.com>
2023-06-12 18:36:12 +0530

    done

--- resources/views/livewire/show-comments.blade.php
+++ resources/views/livewire/show-comments.blade.php
@@ -4,8 +4,11 @@
             <div class="max-w-7xl my-4 mx-auto sm:px-6 lg:px-8">
                 <div class="bg-white flex justify-between dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="pl-6 pr-6 pt-4 text-sm text-gray-900 dark:text-gray-400" style="width: calc(100% - 120px)">
-                        <span class="font-bold">{{ $comment->user->name }}:</span> 
-                        <p>{{ $comment->content }}</p>
+                        <div class="flex items-center pb-1">
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="#BDBDBD" width="16px" height="16px" viewBox="0 0 24 24"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"><path d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10 10-4.48 10-10S17.52 2 12 2zm0 3c1.66 0 3 1.34 3 3s-1.34 3-3 3-3-1.34-3-3 1.34-3 3-3zm0 14.2c-2.5 0-4.71-1.28-6-3.22.03-1.99 4-3.08 6-3.08 1.99 0 5.97 1.09 6 3.08-1.29 1.94-3.5 3.22-6 3.22z"></path></g></svg>
+                            <span class="font-bold pl-1">{{ $comment->user->name }}:</span> 
+                        </div>
+                        <p class="pl-6">{{ $comment->content }}</p>
                     </div>
                     @if($thread->user_id == Auth::id())
                         <form>
